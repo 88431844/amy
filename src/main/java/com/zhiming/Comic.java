63b5ef9b32567ca374b6d29da2e9d01e0a91732c
mayuan <mayuanucas@qq.com>
2018-07-28 00:57:37 +0800

    Kukudm爬虫

--- src/main/java/com/zhiming/Comic.java
+++ src/main/java/com/zhiming/Comic.java
@@ -1,16 +1,20 @@
 package com.zhiming;
 
+import com.zhiming.comic.Kukudm;
 import picocli.CommandLine;
 import picocli.CommandLine.*;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import us.codecraft.webmagic.Spider;
 
 /**
  * @author: mayuan
  * @desc:
  * @date: 2018/07/24
  */
-@Command(name = "amy", mixinStandardHelpOptions = true, version = "amy version 0.1", description = "A fast and elegant comic crawler -> (http://comic.kukudm.com/)")
+@Command(name = "amy", mixinStandardHelpOptions = true,
+        version = "amy version 0.1",
+        description = "A fast and elegant comic crawler -> (http://comic.kukudm.com)")
 public class Comic implements Runnable {
 
     @Option(names = {"-o", "--output"}, required = true, defaultValue = "./download", description = "存储位置")
@@ -21,8 +25,12 @@ public class Comic implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("输出位置:" + outputPath);
-        System.out.println("爬取页面为:" + startUrl);
+        Spider.create(new Kukudm(outputPath))
+                .addUrl(startUrl)
+                .thread(4)
+                .run();
+
+        System.out.println("所有章节采集完毕,准备退出...");
     }
 
     public static void main(String[] args) {
